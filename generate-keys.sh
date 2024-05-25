066571ba3048968af0dae2ad22ab50688f54f5ea
someone5678 <someone5678@users.noreply.github.com>
2024-05-25 18:24:02 +0900

    parasite-signatures: Add scripts
    Change-Id: I581eb1466a7afd69a04f3425feb221488f3d5b3d

---
+++ generate-keys.sh
@@ -0,0 +1,24 @@
+#!/bin/sh
+
+AVBROOT=../../../prebuilts/tools-parasite/avbroot/avbroot
+
+subject=$1
+if [ -z "$subject" ]
+then
+    echo -e "Subject not specified!"
+    exit 1
+fi
+
+outdir=$2
+if [ -z "$outdir" ]
+then
+    echo -e "Output dir not specified!"
+    exit 1
+fi
+
+for x in bluetooth cts_uicc_2021 sdk_sandbox releasekey platform shared media networkstack testkey; do \
+    ./make_key $outdir/$x "$subject" rsa; \
+done
+
+avbroot key extract-avb -k $outdir/releasekey-private.pem -o $outdir/avb_pkmd.bin
+avbroot key generate-cert -k $outdir/releasekey-private.pem -o $outdir/ota.crt
