d697af5a57e757d4f6ba400a81775a53802c22a5
Saikrishna1504 <saikrishna26918@gmail.com>
2024-05-26 03:52:59 +0000

    Scripts: Update generate-keys.sh

--- generate-keys.sh
+++ generate-keys.sh
@@ -1,23 +1,27 @@
 #!/bin/sh
 
 AVBROOT=.bin/avbroot
-
 subject=$1
-if [ -z "$subject" ]
-then
-    echo -e "Subject not specified!"
+
+if [ -z "$subject" ]; then
+    echo "Subject not specified!"
     exit 1
 fi
 
 outdir=$2
-if [ -z "$outdir" ]
-then
-    echo -e "Output dir not specified!"
+if [ -z "$outdir" ]; then
+    echo "Output dir not specified!"
     exit 1
 fi
 
-for x in bluetooth cts_uicc_2021 sdk_sandbox releasekey platform shared media networkstack testkey; do \
-    ./make_key $outdir/$x "$subject" rsa; \
+if [ ! -d "$outdir" ]; then
+    mkdir -p "$outdir"
+else
+    rm -f "$outdir"/*
+fi
+
+for x in bluetooth cts_uicc_2021 sdk_sandbox releasekey platform shared media networkstack testkey; do
+    ./make_key $outdir/$x "$subject" rsa
 done
 
 $AVBROOT key extract-avb -k $outdir/releasekey-private.pem -o $outdir/avb_pkmd.bin
