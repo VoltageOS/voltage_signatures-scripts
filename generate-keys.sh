18b02cf93c170920ec17987b2f41e7a13d859395
Saikrishna1504 <saikrishna26918@gmail.com>
2024-05-27 13:24:42 +0000

    Update generate-keys.sh
    Signed-off-by: Saikrishna1504 <saikrishna26918@gmail.com>

--- generate-keys.sh
+++ generate-keys.sh
@@ -14,15 +14,31 @@ if [ -z "$outdir" ]; then
     exit 1
 fi
 
+# Ensure the output directory exists
 if [ ! -d "$outdir" ]; then
     mkdir -p "$outdir"
-else
-    rm -f "$outdir"/*
 fi
 
-for x in bluetooth cts_uicc_2021 sdk_sandbox releasekey platform shared media networkstack testkey; do
-    ./make_key $outdir/$x "$subject" rsa
+# List of key names
+keys="bluetooth cts_uicc_2021 sdk_sandbox releasekey platform shared media networkstack testkey"
+
+# Function to remove existing files
+remove_existing_files() {
+    key_base=$1
+    rm -f "$outdir/$key_base.pk8"
+    rm -f "$outdir/$key_base.x509.pem"
+}
+
+# Remove existing files for each key
+for key in $keys; do
+    remove_existing_files "$key"
 done
 
-$AVBROOT key extract-avb -k $outdir/releasekey-private.pem -o $outdir/avb_pkmd.bin
-$AVBROOT key generate-cert -k $outdir/releasekey-private.pem -o $outdir/ota.crt
+# Generate keys
+for key in $keys; do
+    ./make_key "$outdir/$key" "$subject" rsa
+done
+
+# Extract and generate AVB-related files, replacing if they exist
+$AVBROOT key extract-avb -k "$outdir/releasekey-private.pem" -o "$outdir/avb_pkmd.bin"
+$AVBROOT key generate-cert -k "$outdir/releasekey-private.pem" -o "$outdir/ota.crt"
